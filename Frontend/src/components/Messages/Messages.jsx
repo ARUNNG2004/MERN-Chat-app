0178224e19e8864faf37f09ee43f22f744b940c8
ARUNNG2004 <arun1990govind@gmail.com>
2024-11-10 09:54:23 +0530

    Fixed path

--- Frontend/src/components/Messages/Messages.jsx
+++ Frontend/src/components/Messages/Messages.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import useGetMessages from "../../hooks/useGetMessages";
+import useGetMessages from "../../Hooks/useGetMessages";
 import MessageSkeleton from "../../skeletons/MessageSkeleton.jsx";
 import Message from "./Message";
 import useListenMessages from "../../Hooks/useListenMessages.js";
